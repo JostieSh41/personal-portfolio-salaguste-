1d798335eb8b0b26bfe35d2d9a86c5ab42909de4
JostieSh41 <138366262+JostieSh41@users.noreply.github.com>
2024-05-21 00:57:24 +0800

    Add files via upload

---
+++ MyPortfolio/portfolio.js
@@ -0,0 +1,38 @@
+var tablinks = document.getElementsByClassName("tab-links");
+var tabcontents = document.getElementsByClassName("tab-contents");
+
+function opentab(tabname){
+    for(tablink of tablinks){
+        tablink.classList.remove("active-link")
+    }
+    for(tabcontent of tabcontents){
+        tabcontent.classList.remove("active-tab")
+    }
+    event.currentTarget.classList.add("active-link");
+    document.getElementById(tabname).classList.add("active-tab");
+}
+
+var sidemenu = document.getElementById("sidemenu");
+
+function openmenu(){
+    sidemenu.style.right = "0"
+}
+function closemenu(){
+    sidemenu.style.right = "-200px"
+}
+
+const form = document.querySelector('form');
+
+form.addEventListener('submit', (event) => {
+  event.preventDefault();
+
+  const name = document.getElementById('name').value;
+  const email = document.getElementById('email').value;
+  const message = document.getElementById('message').value;
+
+  // You can add your own logic to handle the form submission, such as sending the data to a server
+  console.log('Form submitted:', { name, email, message });
+
+  // Clear the form fields
+  form.reset();
+});
